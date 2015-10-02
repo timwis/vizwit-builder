fa24a9dd35e7f2b50dedf97bebb8ef7821c95e61
timwis <tim@timwis.com>
2015-10-02 11:26:57 -0700

    Rebuild with html and js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,26 @@
-var editor = new JSONEditor(document.getElementById('page-content'), {
-	ajax: true,
-	schema: {
-		$ref: '../card.json'
-	},
-	theme: 'bootstrap3',
-	iconlib: 'fontawesome4',
-	disable_collapse: true
+var $ = require('jquery')
+var serialize = require('form-serialize')
+
+// On form submit
+$('form').on('change', function(e) {
+	var input = serialize(this, {hash: true})
+	$('#json').val(JSON.stringify(input, null, 4))
+	e.preventDefault()
+})
+
+// When chart type changes
+$('#chartType').on('change', function(e) {
+	if(this.value) {
+		$('[data-chart-type]').hide()
+		$('[data-chart-type="' + this.value + '"]').show()
+	}
+})
+
+// When result box changes
+$('#json').on('change', function(e) {
+	var json = JSON.parse(this.value)
+	var i
+	for(i in json) {
+		$('#' + i).val(json[i])
+	}
 })
